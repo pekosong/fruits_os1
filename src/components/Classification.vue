228ee3be129d8e62613c588971193142b58e7eee
pekosong <peko22@naver.com>
2019-04-04 18:28:06 +0900

    'song'

--- src/components/Classification.vue
+++ src/components/Classification.vue
@@ -28,6 +28,14 @@
                 v-show="true"
               ></canvas>
               <canvas ref="canvas1" id="canvas1" width="800px" height="600px" v-show="false"></canvas>
+              <canvas
+                style="border-radius: 10px;"
+                ref="cropfruits"
+                id="cropfruits"
+                width="300px"
+                height="300px"
+                v-show="false"
+              ></canvas>
             </div>
           </div>
         </div>
@@ -35,99 +43,99 @@
 
         <!-- Right -->
         <div class="right">
-          <!-- Right-Top -->
-          <div class="right-top">
-            <div>
-              <h4>선별결과 : {{result}} {{parseInt(prob * 100)}}%</h4>
-            </div>
-
-            <div style="text-align:center">
-              <canvas
-                style="border-radius: 10px;"
-                ref="canvas2"
-                id="canvas2"
-                width="300px"
-                height="300px"
-                v-show="true"
-              ></canvas>
-            </div>
-          </div>
-          <!-- Right-Top-End -->
-
           <!-- Right-Bot -->
-          <div>
-            <h4>등급 선별 결과</h4>
-          </div>
-
-          <b-container class="right-bot">
-            <b-row style="width:100%">
-              <b-col class="quality text-center">
-                <div>
-                  <h3>흠집</h3>
-                </div>
-                <div>
-                  <img
-                    :src="selectedimg"
-                    :style="{filter:'grayscale(' + (100 - (100 / 1 * quality)) + '%)'}"
-                    style="width: 80px"
-                  >
-                  <div>
-                    <b-button v-if="quality" variant="info" size="lg">정상</b-button>
-                    <b-button v-else variant="danger" size="lg">비정상</b-button>
-                  </div>
-                </div>
-              </b-col>
-              <b-col class="color text-center">
-                <div>
-                  <h3>색깔</h3>
-                </div>
-                <div>
-                  <img
-                    :src="selectedimg"
-                    :style="{filter:'grayscale(' + (100 - (100 / 1 * color)) + '%)'}"
-                    style="width: 80px"
-                  >
-                  <div>
-                    <b-button v-if="color" variant="info" size="lg">정상</b-button>
-                    <b-button v-else variant="danger" size="lg">비정상</b-button>
-                  </div>
-                </div>
-              </b-col>
-              <b-col class="size text-center">
-                <div>
-                  <h3>크기</h3>
-                </div>
-                <div>
-                  <img
-                    :src="selectedimg"
-                    :style="{filter:'grayscale(' + 0 + '%)'}"
-                    style="width: 80px"
-                  >
-                  <div>
-                    <b-button variant="info" size="lg">{{size}}cm</b-button>
-                  </div>
-                </div>
-              </b-col>
-            </b-row>
-          </b-container>
-          <!-- Right-Bot-End -->
-
           <div>
             <h4>등급별 선별 현황</h4>
           </div>
-          <b-container class="right-bot">
+          <b-container class="right-top">
             <b-row class="results">
               <b-col v-for="n in numbers" :key="n" v-show="show" class="center">
                 <img
                   :src="selectedimg"
                   :style="{filter:'grayscale(' + (100 - (100 / n)) + '%)'}"
-                  style="width: 80px;"
+                  style="width: 100px;"
                 >
                 <b-button
                   size="lg"
                   block
                   variant="dark"
                 >{{ classes[numbers][n-1] }} - {{ count[numbers][n-1]}} 개</b-button>
+              </b-col>
+            </b-row>
+          </b-container>
+
+          <div>
+            <h4>등급 선별 결과</h4>
+          </div>
+          <b-container class="right-bot">
+            <b-row>
+              <b-col v-show="show" style="width: 670px">
+                <b-card
+                  v-for="(item, index) in oldtracker.slice().reverse().slice(0,5)"
+                  :key="index"
+                  no-body
+                  class="mb-1"
+                  style="max-height: 120px; border-bottom: 1px solid black;"
+                >
+                  <b-row no-gutters class="p-1">
+                    <b-col style="margin-top:2px" md="2">
+                      <b-card-img :src="item['img']" class="rounded-0"></b-card-img>
+                    </b-col>
+                    <b-col md="10">
+                      <b-row align-h="center" class="text-center">
+                        <b-col style="margin-top:15px" md="2">
+                          <div>
+                            <h4>ID:{{item["id"]}}</h4>
+                          </div>
+                          <div>
+                            <h3>{{item["grade"]}}</h3>
+                          </div>
+                        </b-col>
+                        <b-col md="3">
+                          <div>흡집</div>
+                          <div>
+                            <img
+                              :src="selectedimg"
+                              :style="{filter:'grayscale(' + (100 - (100 / 1 * quality)) + '%)'}"
+                              style="width: 45px"
+                            >
+                            <div>
+                              <b-button size="sm" v-if="quality" variant="info">정상</b-button>
+                              <b-button size="sm" v-else variant="danger">비정상</b-button>
+                            </div>
+                          </div>
+                        </b-col>
+                        <b-col md="3">
+                          <div>색깔</div>
+                          <div>
+                            <img
+                              :src="selectedimg"
+                              :style="{filter:'grayscale(' + (100 - (100 / 1 * color)) + '%)'}"
+                              style="width: 45px"
+                            >
+                            <div>
+                              <b-button size="sm" v-if="color" variant="info">정상</b-button>
+                              <b-button size="sm" v-else variant="danger">비정상</b-button>
+                            </div>
+                          </div>
+                        </b-col>
+                        <b-col md="3">
+                          <div>크기</div>
+                          <div>
+                            <img
+                              :src="selectedimg"
+                              :style="{filter:'grayscale(' + 0 + '%)'}"
+                              style="width: 45px"
+                            >
+                            <div>
+                              <b-button size="sm" variant="info">{{item['size']}}cm</b-button>
+                            </div>
+                          </div>
+                        </b-col>
+                      </b-row>
+                    </b-col>
+                  </b-row>
+                </b-card>
               </b-col>
             </b-row>
           </b-container>
@@ -175,8 +183,10 @@ export default {
       size: 0,
       quality: 1,
       color: 1,
-      tracker: {},
-      idx: 1
+      tracker: [],
+      oldtracker: [],
+      idx: 1,
+      show: true
     };
   },
   mounted() {
@@ -265,17 +275,28 @@ export default {
     // Canvas2에 Detecting된 과일 분류
     classification(box) {
       try {
-        const x = parseInt(box.left);
-        const y = parseInt(box.top);
+        const left = parseInt(box.left);
+        const top = parseInt(box.top);
         const width = parseInt(box.width);
         const height = parseInt(box.height);
 
-        this.$refs.canvas2
+        this.$refs.cropfruits
           .getContext("2d")
-          .drawImage(this.$refs.canvas1, x, y, width, height, 0, 0, 300, 300);
+          .drawImage(
+            this.$refs.canvas1,
+            left,
+            top,
+            width,
+            height,
+            0,
+            0,
+            300,
+            300
+          );
+        const img = this.$refs.cropfruits.toDataURL("image/png");
 
         const inputImage = tf.browser
-          .fromPixels(this.$refs.canvas2)
+          .fromPixels(this.$refs.cropfruits)
           .resizeNearestNeighbor([224, 224])
           .toFloat()
           .div(tf.scalar(255))
@@ -286,7 +307,6 @@ export default {
         const result = this.classes[this.numbers][
           results.indexOf(Math.max(...results))
         ];
-
         this.quality = result == "A급" ? 1 : 0;
         this.prob = Math.max(...results);
 
@@ -305,11 +325,10 @@ export default {
             this.count[this.numbers][2] += 1;
           }
         }
-        return result;
+        return [result, img];
       } catch (err) {
         console.log(err);
       }
-
       this.show = false;
       this.show = true;
     },
@@ -318,101 +337,98 @@ export default {
       this.yolomodel = null;
       this.mobilenet = null;
     },
-    maketracker(pos, cls, box) {
+    maketracker(pos, cls, box, now) {
       this.tracker[this.idx] = {};
       this.tracker[this.idx]["id"] = this.idx;
+
+      const cx = box.left - box.width / 2;
+      const cy = box.top - box.height / 2;
+      this.tracker[this.idx]["last_pos"] = { cx: cx, cy: cy };
+
+      this.tracker[this.idx]["day"] = `${now.getMonth() +
+        1}월 ${now.getDate()}일`;
+      this.tracker[this.idx][
+        "hr"
+      ] = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
       this.tracker[this.idx]["pos"] = pos;
-      this.tracker[this.idx]["cls"] = cls;
+      this.tracker[this.idx]["come"] = 0;
       this.tracker[this.idx]["dis"] = 0;
+      this.tracker[this.idx]["cls"] = cls;
       this.tracker[this.idx]["gone"] = false;
       this.tracker[this.idx]["updated"] = false;
       this.tracker[this.idx]["box"] = box;
       this.tracker[this.idx]["grade"] = "선별중";
-      this.tracker[this.idx]["size"] = "측정중";
+      this.tracker[this.idx]["img"] = "";
+
+      this.tracker[this.idx]["size"] = parseInt(
+        Math.min(box.width, box.height) / 20
+      );
       this.idx += 1;
+      (this.show = false), (this.show = true);
     },
     caldistance(obj1, obj2) {
-      const x1 = obj1["x"];
-      const y1 = obj1["y"];
-      const x2 = obj2["x"];
-      const y2 = obj2["y"];
+      const cx1 = obj1["cx"];
+      const cy1 = obj1["cy"];
+      const cx2 = obj2["cx"];
+      const cy2 = obj2["cy"];
 
       // obj1과 obj2의 거리를 구함
       return Math.sqrt(
-        Math.pow(Math.abs(x2 - x1), 2) + Math.pow(Math.abs(y2 - y1), 2)
+        Math.pow(Math.abs(cx2 - cx1), 2) + Math.pow(Math.abs(cy2 - cy1), 2)
       );
     },
 
     // Canvas의 Detecting된 과일에 Rectangle과 Label
-    draw(box, id, cls, grade, size) {
-      const x = parseInt(box.left);
-      const y = parseInt(box.top);
-      const width = parseInt(box.width);
-      const height = parseInt(box.height);
-
-      this.size = parseInt(Math.min(width, height) / 20);
-
-      this.$refs.canvas2
-        .getContext("2d")
-        .drawImage(this.$refs.canvas1, x, y, width, height, 0, 0, 300, 300);
-
+    draw(box,  cls, id, grade, size) {
       const ctx = this.$refs.canvas.getContext("2d");
       ctx.font = "30px sans-serif";
-      ctx.fillText(`id:${id}-${cls} `, x, y - 10);
+      ctx.fillText(`id:${id}-${cls} `, box.left, box.top - 10);
       ctx.font = "30px sans-serif";
-      ctx.fillText(`${grade}`, x + width / 2 - 25, y + height / 2 - 20);
-      ctx.fillText(`${size}cm`, x + width / 2 - 15, y + height / 2 + 20);
+      ctx.fillText(`${grade}`, box.left + box.width / 2 - 25, box.top + box.height / 2 - 20);
+      ctx.fillText(`${size}cm`, box.leftft + box.width / 2 - 15, box.top + box.height / 2 + 20);
       ctx.lineWidth = "5";
       ctx.strokeStyle = "yellow";
-      ctx.strokeRect(x, y, width, height);
+      ctx.strokeRect(box.left, box.top, box.width, box.height);
     },
 
     finddistance(afters, boxes) {
-      let befores = [];
-
+      const befores = [];
       const dists = [];
       const new_afters = [];
       const new_boxes = [];
-
       let dst = [];
-      let last = {};
 
       // tracker들의 각 마지막 위치를 구함
       Object.keys(this.tracker).forEach(key => {
-        if (this.tracker[key]["gone"] == false) {
-          Object.keys(this.tracker[key]["pos"]).forEach(i => {
-            last = this.tracker[key]["pos"][i];
-          });
-        }
+        const last = this.tracker[key]["last_pos"];
         befores.push(last);
       });
-      console.log(befores.length);
-      console.log(befores);
+
       // 가장 가까운 거리를 구함
       befores.forEach(before => {
         afters.forEach(after => {
           dst.push(this.caldistance(before, after));
         });
-        let idx = dst.indexOf(Math.min(...dst));
+        const idx = dst.indexOf(Math.min(...dst));
 
         new_afters.push(afters[idx]);
         new_boxes.push(boxes[idx]);
         dists.push(dst[idx]);
-
         dst = [];
       });
+
       // 가장 가까운 거리들 다음 위치와 박스 그리고 거리를 반환
       return [new_afters, new_boxes, dists];
     },
     updateposition(boxes) {
-      if (boxes.length > 0) {
+      try {
         const afters = [];
 
         // boxes에서 cx, cy 값 반환
         boxes.forEach(box => {
-          const x = box.left - box.width / 2;
-          const y = box.top - box.height / 2;
-          afters.push({ x: x, y: y });
+          const cx = box.left - box.width / 2;
+          const cy = box.top - box.height / 2;
+          afters.push({ cx: cx, cy: cy });
         });
 
         // 현재 trackers 내 거리와 새로운 boxes의 거리를 구함
@@ -420,6 +436,7 @@ export default {
         const new_afters = results[0];
         const new_boxes = results[1];
         const new_dist = results[2];
+
         // 거리가 가까운 순으로 정렬된 index 계산
         const len = new_dist.length;
         const indices = new Array(len);
@@ -433,7 +450,7 @@ export default {
         });
 
         // 현재 시각 구함
-        const now = new Date().getTime();
+        const now = new Date();
 
         // tracker의 key를 list로 가져옴
         const ppl = Object.keys(this.tracker);
@@ -442,32 +459,30 @@ export default {
 
         // 가장 가까운 순으로 정렬된 index를 id로 하여 새로운 tracker들을 update
         indices.forEach(id => {
-          if (z < afters.length && z < ppl.length && z < new_boxes.length) {
-            if (
-              !this.tracker[ppl[id]]["updated"] &&
-              !this.tracker[ppl[id]]["gone"]
-            ) {
+          if (z < ppl.length && z < boxes.length) {
+            if (!this.tracker[ppl[id]]["updated"]) {
               this.tracker[ppl[id]]["dis"] = 0;
+              this.tracker[ppl[id]]["come"] += 1;
+              this.tracker[ppl[id]]["last_pos"] = new_afters[id];
               this.tracker[ppl[id]]["pos"][now] = new_afters[id];
               this.tracker[ppl[id]]["box"] = new_boxes[id];
               this.tracker[ppl[id]]["updated"] = true;
+
               this.draw(
-                new_boxes[id],
-                this.tracker[ppl[id]]["id"],
+                this.tracker[ppl[id]]["box"],
                 this.tracker[ppl[id]]["cls"],
+                this.tracker[ppl[id]]["id"],
                 this.tracker[ppl[id]]["grade"],
                 this.tracker[ppl[id]]["size"]
               );
-              if (this.tracker[ppl[id]]["grade"] == "선별중") {
-                this.tracker[ppl[id]]["grade"] = this.classification(
-                  new_boxes[id]
-                );
-                this.tracker[ppl[id]]["size"] = parseInt(
-                  Math.min(new_boxes[id]["width"], new_boxes[id]["height"]) / 20
-                );
+              if (
+                this.tracker[ppl[id]]["grade"] == "선별중" &&
+                this.tracker[ppl[id]]["come"] > 5
+              ) {
+                let results = this.classification(this.tracker[ppl[id]]["box"]);
+                this.tracker[ppl[id]]["grade"] = results[0];
+                this.tracker[ppl[id]]["img"] = results[1];
               }
-
-              // console.log(this.tracker[ppl[id]]);
               z += 1;
             }
           }
@@ -476,35 +491,38 @@ export default {
         Object.keys(this.tracker).forEach(key => {
           if (!this.tracker[key]["updated"]) {
             this.tracker[key]["dis"] += 1;
-            if (this.tracker[key]["dis"] > 10 && !this.tracker[key]["gone"]) {
-              this.tracker[key]["gone"] = true;
-              console.log(this.tracker[key]["id"], "제거");
+            if (this.tracker[key]["dis"] > 10) {
+              console.log("올드에 등록");
+              this.oldtracker.push(this.tracker[key]);
+              console.log(this.oldtracker);
+              delete this.tracker[key];
+              console.log("제거");
             }
           }
-          this.tracker[key]["updated"] = false;
+          try {
+            this.tracker[key]["updated"] = false;
+          } catch (err) {}
         });
 
+        function song(box) {
+          const cx = box.left - box.width / 2;
+          return !xs.includes(cx);
+        }
+
+        let xs = [];
+        new_boxes.forEach(el => xs.push(el.left - el.width / 2));
         if (boxes.length > z) {
-          boxes.splice(z).forEach(box => {
+          boxes.filter(song).forEach(box => {
             console.log("등록");
             const new_tracker = {};
-            const x = box.left - box.width / 2;
-            const y = box.top - box.height / 2;
-
-            new_tracker[now] = { x: x, y: y };
-            if (box.class == this.selectedename) {
-              this.maketracker(new_tracker, box.class, box);
-            }
+            const cx = box.left - box.width / 2;
+            const cy = box.top - box.height / 2;
+            new_tracker[now] = { cx: cx, cy: cy };
+            this.maketracker(new_tracker, box.class, box, now);
           });
         }
-      } else {
-        Object.keys(this.tracker).forEach(key => {
-          this.tracker[key]["dis"] += 1;
-          if (this.tracker[key]["dis"] > 10 && !this.tracker[key]["gone"]) {
-            this.tracker[key]["gone"] = true;
-            console.log(this.tracker[key]["id"], "제거");
-          }
-        });
+      } catch (err) {
+        console.log(err);
       }
     }
   }
@@ -545,16 +563,17 @@ export default {
 
 .right-top {
   padding: 15px;
-  background-color: #fff;
   border-radius: 10px;
-  margin-bottom: 20px;
+  margin-bottom: 10px;
+  background-color: #fff;
 }
 
 .right-bot {
-  padding: 15px;
-  background-color: #fff;
+  min-height: 55vh;
+  width: 100%;
+  padding: 10px;
   border-radius: 10px;
-  margin-bottom: 20px;
+  background-color: #fff;
 }
 
 .bot {
@@ -564,11 +583,9 @@ export default {
 }
 
 .results {
+  margin-left: 0;
   width: 100%;
-  height: 170px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  height: 150px;
 }
 
 .container {
